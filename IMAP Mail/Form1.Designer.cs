9e380af348529cedbddc2afce9a40b0d1376a598
Vladimir <drafterdk@gmail.com>
2017-09-06 08:43:58 +0300

    3 commit

---
+++ IMAP Mail/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace IMAP_Mail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTheme = new System.Windows.Forms.TextBox();
+            this.textBoxSubject = new System.Windows.Forms.TextBox();
+            this.buttonGetMail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxTheme
+            // 
+            this.textBoxTheme.Location = new System.Drawing.Point(66, 21);
+            this.textBoxTheme.Name = "textBoxTheme";
+            this.textBoxTheme.Size = new System.Drawing.Size(462, 20);
+            this.textBoxTheme.TabIndex = 0;
+            // 
+            // textBoxSubject
+            // 
+            this.textBoxSubject.Location = new System.Drawing.Point(66, 62);
+            this.textBoxSubject.Multiline = true;
+            this.textBoxSubject.Name = "textBoxSubject";
+            this.textBoxSubject.Size = new System.Drawing.Size(872, 148);
+            this.textBoxSubject.TabIndex = 0;
+            // 
+            // buttonGetMail
+            // 
+            this.buttonGetMail.Location = new System.Drawing.Point(257, 216);
+            this.buttonGetMail.Name = "buttonGetMail";
+            this.buttonGetMail.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetMail.TabIndex = 1;
+            this.buttonGetMail.Text = "Get mail";
+            this.buttonGetMail.UseVisualStyleBackColor = true;
+            this.buttonGetMail.Click += new System.EventHandler(this.buttonGetMail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 248);
+            this.Controls.Add(this.buttonGetMail);
+            this.Controls.Add(this.textBoxSubject);
+            this.Controls.Add(this.textBoxTheme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTheme;
+        private System.Windows.Forms.TextBox textBoxSubject;
+        private System.Windows.Forms.Button buttonGetMail;
+    }
+}
+
---
+++ IMAP Mail/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailKit.Net.Smtp;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit;
+using MimeKit;
+using System.Net.Mime;
+using System.IO;
+using System.Net;
+
+namespace IMAP_Mail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetMail_Click(object sender, EventArgs e)
+        {
+            using (var client = new ImapClient())
+            {
+                // For demo-purposes, accept all SSL certificates
+                client.ServerCertificateValidationCallback = (s, c, h, k) => true;
+
+                client.Connect("imap.mail.ru", 993, true);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                client.Authenticate("elagin@citpb.ru", "6*2xoGRBBrqu");
+
+                // The Inbox folder is always available on all IMAP servers...
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+                var message = inbox.GetMessage(0);
+                textBoxTheme.Text = message.Subject;
+                textBoxSubject.Text = "Total messages: " + inbox.Count;
+
+
+                foreach (var summary in inbox.Fetch(0, -1, MessageSummaryItems.UniqueId | MessageSummaryItems.BodyStructure))
+                {
+                    if (summary.TextBody != null)
+                    {
+                        // this will download *just* the text/plain part
+                        var text = inbox.GetBodyPart(summary.UniqueId, summary.TextBody);
+                        byte[] binaryData;
+                        string buf = text.ToString().Replace("\n", ""); ;
+                        string base64 = "base64";
+                        string xxx = buf.Substring(buf.IndexOf(base64) + 6);
+                        StreamReader strr = new StreamReader(HttpWebRequest.Create(@"http://api.foxtools.ru/v2/Base64.html?mode=Auto&text=CtCX0LTRgNCw0LLRgdGC0LLRg9C50YLQtSEK0JbQtNGDINC%2B0YLRh9C10YLQsCDQt9CwINCw0LLQ%0D%0As9GD0YHRgi4K0JfQtNC10YHRjCDRiNCw0LHQu9C%2B0L0g0L3QsCDRgdC10L3RgtGP0LHRgNGMLgoK%0D%0ACtChINGD0LLQsNC20LXQvdC40LXQvCwK0JDQu9C10LrRgdC10Lkg0J%2FQsNC90L7QsgrRgtC10Lsu%0D%0AICArNyAoOTAxKSA1MjYtMDEwMwpwYW5vdkBjaXRwYi5ydSDCoArQptC10L3RgtGAINCY0KIg0L%2FQ%0D%0AvtC00LTQtdGA0LbQutC4INCx0LjQt9C90LXRgdCwCtCT0LXQvdC10YDQsNC70YzQvdGL0Lkg0LTQ%0D%0AuNGA0LXQutGC0L7RgAo%3D&file=&url=&return=Text&lang=RU&cp=UTF-8&details=1").GetResponse().GetResponseStream());
+
+                      //  StreamReader strr = new StreamReader(HttpWebRequest.Create(@"http://api.foxtools.ru/v2/Base64.html?mode=Auto&text=" + xxx + "&file=&url=&return=Text&lang=RU&cp=UTF-8&details=1").GetResponse().GetResponseStream()) ;
+
+                        // string buf2 = xxx[4].ToString();
+                        // binaryData = Convert.FromBase64String(xxx.Replace(" ",""));
+
+                        // string unicodeString = Encoding.UTF8.GetString(binaryData);
+                        //  textBoxSubject.Text = "0 - " +  xxx[0].ToString()+ "\n 1 - " + xxx[1].ToString() + "\n 2 - " + xxx[2].ToString() + "\n 3 - " + xxx[3].ToString() + xxx[4].ToString(); 
+                        textBoxSubject.Text = strr.ReadToEnd() ;
+                    }
+
+                    if (summary.HtmlBody != null)
+                    {
+                        // this will download *just* the text/html part
+                        var html = inbox.GetBodyPart(summary.UniqueId, summary.HtmlBody);                                                
+                    }
+
+                    // if you'd rather grab, say, an image attachment... it might look something like this:
+                    if (summary.Body is BodyPartMultipart)
+                    {
+                        var multipart = (BodyPartMultipart)summary.Body;
+
+                        var attachment = multipart.BodyParts.OfType<BodyPartBasic>().FirstOrDefault(x => x.FileName == "logo.jpg");
+                        if (attachment != null)
+                        {
+                            // this will download *just* the attachment
+                            var part = inbox.GetBodyPart(summary.UniqueId, attachment);
+                          
+                        }
+                    }
+                }
+
+                //  Console.WriteLine("Total messages: {0}", inbox.Count);
+                //  Console.WriteLine("Recent messages: {0}", inbox.Recent);
+
+                //for (int i = 0; i < inbox.Count; i++)
+                //{
+                //    var message = inbox.GetMessage(i);
+                //    Console.WriteLine("Subject: {0}", message.Subject);
+                //}
+
+                client.Disconnect(true);
+            }
+        }
+    }
+}
